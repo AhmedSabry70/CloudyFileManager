241009b7049bbfbaa014cd98a24b8f3b8c732bce
Ahmed Sabry <infosabry88@gmail.com>
2025-02-25 21:30:35 +0200

    🔧 Refactor: Add resources, set up dependencies, and organize project structure
    - 📂 Structured project directories for better maintainability
    - 📦 Installed and configured essential dependencies
    - 📑 Added necessary resources for smooth project setup
    - 🚀 Optimized project architecture for scalability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+    'prettier'),
+    {
+      rules: {
+        'tailwindcss/no-custom-classname': 'off', // Allow custom Tailwind classes
+        'no-unknown-animations': 'off', // Prevent false animation warnings
+        'at-rule-no-unknown': [
+          'warn',
+          {
+            ignoreAtRules: ['apply', 'variants', 'responsive', 'screen'],
+          },
+        ],
+        "@typescript-eslint/no-explicit-any": "warn"
+      },
+      settings: {
+        tailwindcss: {
+          callees: ['cn'], // Add support for custom class utilities (if using `clsx` or `cn`)
+        },
+    },
+    
+    },
 ];
 
 export default eslintConfig;
